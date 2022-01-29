204f043357b5472ab0633bd2e46dec16a0f62570
Shivang Kaul <shangkaul@gmail.com>
2022-01-29 08:08:16 +0000

    passport, jwt auth

--- Models/user.js
+++ Models/user.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-	_id: mongoose.Schema.Types.ObjectId,
-  name: String
+  name: String,
+  email: String,
+  password: String
 },{collection:'user'});
 
 module.exports = mongoose.model("User", userSchema);
--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var logger = require('morgan');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
+const passport = require("passport");
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -13,15 +14,20 @@ app.use(bodyParser.json());
 const dbURI = process.env['dbURI'];
 var usersRouter = require('./Routes/users.routes');
 
-app.listen(port);
-console.log('Express app started on port ' + port);
-
 mongoose
 	.connect(dbURI)
 	.then(() => console.log("Database Connected"))
 	.catch((err) => console.log(err));
 
 mongoose.Promise = global.Promise;
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./Config/passport")(passport);
+
+app.listen(port);
+console.log('Express app started on port ' + port);
 
 app.use('/users', usersRouter);
 
