c24c22c3dc02d97d378d4d42d518e60805220bcc
Shivang Kaul <shangkaul@gmail.com>
2022-02-04 06:00:20 +0000

    procfile added for heroku

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const passport = require("passport");
 var cors = require('cors');
 var app = express();
 const port = process.env.PORT || 3000;
+// const socketIo = require("socket.io");
+// var http=require('https');
+
 
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -28,12 +31,38 @@ app.use(passport.initialize());
 // Passport config
 require("./Config/passport")(passport);
 
-app.listen(port);
-console.log('Express app started on port ' + port);
+
 
 app.use('/users', usersRouter);
 app.use('/ws', wsRouter);
 app.use('/task', taskRouter);
+
+// const server = http.createServer(app);
+// const io = socketIo(server); 
+
+// let interval;
+
+// io.on("connection", (socket) => {
+//   console.log("New client connected");
+//   if (interval) {
+//     clearInterval(interval);
+//   }
+//   interval = setInterval(() => getApiAndEmit(socket), 1000);
+//   socket.on("disconnect", () => {
+//     console.log("Client disconnected");
+//     clearInterval(interval);
+//   });
+// });
+
+// const getApiAndEmit = socket => {
+//   const response = new Date();
+//   // Emitting a new message. Will be consumed by the client
+//   socket.emit("FromAPI", response);
+// };
+
+app.listen(port);
+console.log('Express app started on port ' + port);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
